98bf55da8d4b7f9a6d578686d4b4dcc1fde5e4a6
Eclipse95 <9960386+eclipse95@users.noreply.github.com>
2024-07-23 10:31:04 +0200

    update from upstream + init.py

---
+++ lib/__init__.py
@@ -0,0 +1,3 @@
+from .ahoi.modem import packet, modem
+from .ahoi.com import streamer, socket, serial, base
+from .ahoi.handlers import SampleHandler, Handler
---
+++ lib/ahoi/__init__.py
@@ -0,0 +1,3 @@
+from .com import streamer, socket, base, serial
+from .handlers import Handler, SampleHandler
+from .modem import packet, modem
